ff3105a3e3d1617f38a3ab9a21faeb7bf0e975ab
gavlaaaaaaaa <gavlaaaaa5@googlemail.com>
2015-03-05 17:57:16 +0000

    Initial Commit

---
+++ Virtual68HC11/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Virtual68HC11
+//
+//  Created by Lewis Gavin on 05/03/2015.
+//  Copyright (c) 2015 GavPlusPlus. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
