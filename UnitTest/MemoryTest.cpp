66ad05a43a0e50eda7cec91386db30eab8f7eade
gavlaaaaaaaa <gavlaaaaa5@googlemail.com>
2015-03-05 18:45:34 +0000

    Project Skeleton set-up Test framework added and set up correctly

---
+++ UnitTest/MemoryTest.cpp
@@ -0,0 +1,17 @@
+//
+//  MemoryTest.cpp
+//  Virtual68HC11
+//
+//  Created by Lewis Gavin on 05/03/2015.
+//  Copyright (c) 2015 GavPlusPlus. All rights reserved.
+//
+
+#include "MemoryTest.h"
+
+TEST_F (MemoryTest, DUMMY) {
+    dummy_test();
+}
+void MemoryTest::dummy_test(){
+
+    EXPECT_TRUE(true);
+}
---
+++ UnitTest/MemoryTest.h
@@ -0,0 +1,21 @@
+//
+//  MemoryTest.h
+//  Virtual68HC11
+//
+//  Created by Lewis Gavin on 05/03/2015.
+//  Copyright (c) 2015 GavPlusPlus. All rights reserved.
+//
+
+#ifndef __Virtual68HC11__MemoryTest__
+#define __Virtual68HC11__MemoryTest__
+
+#include "gtest/gtest.h"
+
+class MemoryTest : public ::testing::Test {
+    
+public:
+    void dummy_test();
+    
+};
+
+#endif /* defined(__Virtual68HC11__MemoryTest__) */
